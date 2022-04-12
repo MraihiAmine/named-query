dd86cc5027ae2f70b57dc980724f66d788075993
Mraihi Amine <mraihiamin@gmail.com>
2022-04-12 14:53:41 +0100

    First

---
+++ src/test/java/com/namedquery/namedquery/NamedQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namedquery.namedquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamedQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
